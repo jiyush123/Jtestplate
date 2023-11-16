384251845c5c5def427c2db3eb647124c1a3e4f9
jianys <jianys@chinaunicom.cn>
2023-11-16 20:51:41 +0800

    修改登录时间字段

--- testplate_server/models.py
+++ testplate_server/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     password = models.CharField(max_length=64, verbose_name='密码', blank=False)
     created_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     updated_time = models.DateTimeField(verbose_name='修改时间')
-    login_time = models.DateTimeField(verbose_name='登录时间')
+    login_time = models.DateTimeField(verbose_name='登录时间', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -19,7 +19,7 @@ class Token(models.Model):
     """这是token表"""
     token = models.CharField(max_length=256, verbose_name='token', blank=False)
     user = models.ForeignKey(to="User", to_field="id", on_delete=models.CASCADE, verbose_name='关联用户', blank=False)
-    created_time = models.DateTimeField(verbose_name='创建时间',auto_now_add=True)
+    created_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     timeout_time = models.DateTimeField(verbose_name='失效时间',
                                         blank=True,
                                         null=True)
