615dd6a24099adb8db8bb7e65245ba314d1c604c
jianys <jianys@chinaunicom.cn>
2023-11-25 17:10:21 +0800

    增加列表序列化对象和视图

--- testplate_server/apicaseviews.py
+++ testplate_server/apicaseviews.py
@@ -13,13 +13,24 @@ from testplate_server.models import APICase, APICaseStep
 class APICaseAddSerializer(serializers.ModelSerializer):
     class Meta:
         model = APICase
-        exclude = ["status", "updated_time"]
+        exclude = ["status", "updated_time", "last_time"]
 
 
 class APICaseStepAddSerializer(serializers.ModelSerializer):
     class Meta:
         model = APICaseStep
         fields = "__all__"
+
+
+class APICaseListSerializer(serializers.ModelSerializer):
+    status = serializers.CharField(source='get_status_display')
+    level = serializers.CharField(source='get_level_display')
+    result = serializers.CharField(source='get_result_display')
+    last_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        model = APICase
+        fields = ["id", "name", "level", "status", "result", "last_time"]
 
 
 class APICaseAdd(APIView):
@@ -58,3 +69,37 @@ class APICaseAdd(APIView):
                'code': '200',
                'msg': "添加成功"}
         return Response(res)
+
+
+class APICaseList(APIView):
+    """获取api测试用例列表接口"""
+
+    def get(self, request):
+        sq = request.GET
+        excluded_keys = ['size', 'page']
+        filtered_params = {k: v for k, v in sq.items() if k not in excluded_keys}
+        # 获取列表的查询字段后，根据需要进行模糊查询
+        if 'name' in filtered_params:
+            filtered_params['name__contains'] = filtered_params['name']
+            filtered_params.pop('name')
+
+        api_info = APICase.objects.filter(**filtered_params)
+        size = int(request.GET.get('size'))
+        page = int(request.GET.get('page'))
+        queryset = api_info.annotate(count=Count('id')).order_by('-id')
+
+        # slice方法进行分页
+        start = (page - 1) * size
+        end = start + size
+        queryset = queryset[start:end]
+        serializer = APICaseListSerializer(instance=queryset, many=True)
+
+        result = {
+            'status': True,
+            'code': 200,
+            'data': serializer.data,
+            'total': api_info.count(),
+            'page': page,
+            'size': size
+        }
+        return Response(result)
