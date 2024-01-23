07d49c3a78fa908aa1d6388b2201cad21cc16653
jianys <jianys@chinaunicom.cn>
2024-01-23 10:41:04 +0800

    优化提取参数方法

--- testplate_server/utils/extract_params.py
+++ testplate_server/utils/extract_params.py
@@ -10,10 +10,18 @@ def replace_placeholder(match, ext_params):
 
 # 使用正则表达式和上面定义的函数替换字符串
 def extract_func(param, ext_params):
-    # 判断传入的参数是否为字符串，只有字符串才能使用正则替换
+    # 判断传入的参数是否为字符串，只有字符串才使用正则替换
     if isinstance(param, str):
-        output_string = re.sub(r'\$\{(\w+)\}', lambda m: replace_placeholder(m, ext_params), param)
-        return output_string
+        if len(param) >= 4:
+            # 如果字符串只有一个${}没有其它字符,提取中间字符串匹配对应值，直接替换
+            # 第一个第二个为'${'时，往后找第一个}，如果}后面没有字符串，则匹配
+            if param[0] == '$' and param[1] == '{' and param[-1] == '}' and '}' not in param[2:-1]:
+                output_params = ext_params[param[2:-1]]  # 直接拿提取的字符串匹配键值对，拿到值直接返回
+                return output_params
+            else:
+                # 替换字符串
+                output_params = re.sub(r'\$\{(\w+)\}', lambda m: replace_placeholder(m, ext_params), param)
+                return output_params
     else:
         return param
 
