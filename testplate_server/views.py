26761bf73c151176d20f703526464a0342d32294
jianys <jianys@chinaunicom.cn>
2024-03-27 17:01:12 +0800

    注释代码

--- testplate_server/views.py
+++ testplate_server/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from rest_framework import serializers
+# from django.shortcuts import render
+# from rest_framework import serializers
 from rest_framework.response import Response
 # Create your views here.
 from rest_framework.views import APIView
 
 
 class TestView(APIView):
-    def get(self,request):
+    def get(self, request):
         name = request.GET.get('name')
-        res = {'code':200,'status':True,'name':name}
+        res = {'code': 200, 'status': True, 'name': name}
         return Response(res)
-
